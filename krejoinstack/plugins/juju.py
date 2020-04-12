c044a68507b6976d2b195609d0a75744b8305a5e
Erlon R. Cruz <erlon@canonical.com>
2020-04-12 09:52:40 -0300

    Adds support for template files on juju

--- krejoinstack/plugins/juju.py
+++ krejoinstack/plugins/juju.py
@@ -14,8 +14,10 @@
 #    under the License.
 
 import argparse
+import yaml
 import re
 import time
+import sys
 
 import krejoinstack.plugins.base as base
 import krejoinstack.utils.ssh as ssh
@@ -68,33 +70,33 @@ class JujuSessions(base.KRJPlugin):
         return machines, units
 
     def spawn(self):
+        def _spawn_targets(_targets):
+            for target_dict in _targets:
+                target = list(target_dict.keys())[0]
+                shells = list(target_dict.values())[0].get('shells', None)
+                if not shells:
+                    self.konsole.new_konsole(target)
+                    self.konsole.shells[target].ssh_connect(
+                        self.host, user=self.bastion_user)
+                    self.konsole.run(target, 'juju ssh %s' % target)
+                else:
+
+                    for command_name, command in shells.items():
+                        shell_name = "%s: %s" % (target, command_name)
+                        self.konsole.new_konsole(shell_name)
+                        self.konsole.shells[shell_name].ssh_connect(
+                            self.host, user=self.bastion_user)
+                        self.konsole.run(shell_name, 'juju ssh %s' % target)
+                        self.konsole.run(shell_name, command)
+
         def _filter_units():
-
-            parsed_res = []
-            for opt_name, re_string in [
-                ("--oregex", self.configs.oregex),
-                ("--noregex", self.configs.noregex)]:
-                if re_string:
-                    print("Compiling RE: %s" % re_string)
-                    try:
-                        parsed_re = re.compile(re_string)
-                        print("parsed_re: %s" % parsed_re)
-                        parsed_res.append(parsed_re)
-                    except Exception as e:
-                        print("Error parsing %s" % opt_name)
-                        print(e)
-
             try:
-                open_re = parsed_res[0]
-            except Exception:
-                open_re = None
-                pass
-
-            try:
-                nopen_re = parsed_res[1]
-            except Exception:
-                nopen_re = None
-                pass
+                open_re = re.compile(self.configs.regex)
+                print("parsed_re: %s" % open_re)
+            except Exception as e:
+                print("Error parsing --regex: %s" % self.configs.regex)
+                print(e)
+                sys.exit(1)
 
             fitered_units = []
             for unit in self.units:
@@ -103,41 +105,76 @@ class JujuSessions(base.KRJPlugin):
                 print("match: %s" % open_re.match(unit))
                 if open_re and open_re.match(unit):
                     fitered_units.append(unit)
-                if nopen_re and nopen_re.match(unit):
-                    fitered_units.remove(unit)
 
             return fitered_units
 
-        filtered = _filter_units()
-        if len(filtered) >= 1:
-            for unit in filtered:
-                self.konsole.new_konsole(unit)
-                self.konsole.shells[unit].ssh_connect(
-                    self.host, user=self.bastion_user)
-                self.konsole.run(unit, 'juju ssh %s' % unit)
+        targets = []
+        if self.configs.template:
+            print("Parsing template file: %s" % self.configs.template)
+            stream = open(self.configs.template, 'r')
+            json_template = yaml.load(stream, Loader=yaml.FullLoader)
+
+            sessions = json_template.get('sessions', None)
+            if not sessions:
+                print("Malformated template file. 'sessions' not defined")
+                sys.exit(1)
+
+            template_units = sessions.get('units', [])
+            template_machines = sessions.get('machines', [])
+
+            for template_target in template_units + template_machines:
+                unit_stanza = list(template_target.keys())[0]
+                try:
+                    compiled_stanza = re.compile(unit_stanza)
+                    print("parsed_re: %s" % compiled_stanza)
+                except Exception as e:
+                    print("Error in stanza definition parsing %s" % unit_stanza)
+                    print(e)
+                    sys.exit(1)
+
+                for model_target in self.units + self.machines:
+                    if compiled_stanza.match(model_target):
+                        tgt_shells = list(template_target.values())[0]
+                        tgt = {model_target: tgt_shells}
+                        targets.append(tgt)
+        elif self.configs.regex:
+            filtered = _filter_units()
+            if len(filtered) >= 1:
+                for unit in filtered:
+                    tgt = {unit: {}}
+                    targets.append(tgt)
+        else:
+            for unit in self.units:
+                tgt = {unit: {}}
+                targets.append(tgt)
+
+        if len(targets) > 0:
+            _spawn_targets(targets)
+        else:
+            print("Can't find any target unit or machine to open")
+            sys.exit(1)
 
     @staticmethod
     def add_args(parser):
         juju_parameters = parser.add_argument_group(JujuSessions.name)
+        # Optional: by default will use the model in context
         juju_parameters.add_argument(
             "--model", help='The model to search for the units/machines.',
             metavar='<model name>')
+        # Optional: By default will open 1 tab for each selected unit/machine
         juju_parameters.add_argument(
-            "--oregex", help='Open only units matching this regex.',
-            metavar='<regex>')
-        juju_parameters.add_argument(
-            "--noregex",
-            help='Open only units not mathiching this regex.',
-            metavar='<regex>')
-        juju_parameters.add_argument(
-            "--num-services", help='Opens N tabs for each service.',
+            "--num-tabs", help='Opens N tabs for each service unit or machine.',
             metavar='<number of services>')
+        # Optional: By default will open all tabs in one window. If set, each
+        # application will be opened in one window.
         juju_parameters.add_argument(
             "--group-services", action='store_true',
             help='Group similar services into distict windows.')
-        juju_parameters.add_argument(
-            "--machines", metavar='<list of juju machines>',
-            help='Also logs into the listed machines.')
+        # Optional: Loads from a template YAML file
         juju_parameters.add_argument(
             "--template", metavar='<template file path>',
             help='Loads Juju shells based on this template.')
+        # Optional: Opens only the units/machines matching to regex
+        juju_parameters.add_argument(
+            "--regex", help='Open only units matching this regex.',
+            metavar='<regex>')
