cef77b82c1d500c39c24ef845f1e6853e724dabb
Erlon R. Cruz <erlon@canonical.com>
2021-07-23 16:29:43 -0300

    Rename script to install dependencies

--- tools/install_build_deps.sh
+++ tools/install_build_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install -y --reinstall python3-distutils python3-apt python3 \
-  build-essential libssl-dev libffi-dev python3-dev python3-setuptools python3-venv
+  build-essential libssl-dev libffi-dev python3-dev python3-setuptools \
+  python3-venv python3-pip
 
 pip3 install -q build
